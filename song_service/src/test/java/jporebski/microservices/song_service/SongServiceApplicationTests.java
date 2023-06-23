c5c7c8afcd63032fb7af22bae0a19d4844e72add
Jakub Porebski <jakub_porebski@epam.com>
2023-06-23 11:45:17 +0200

    Hello world! Initial commit of just skeleton services

---
+++ song_service/src/test/java/jporebski/microservices/song_service/SongServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jporebski.microservices.song_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
