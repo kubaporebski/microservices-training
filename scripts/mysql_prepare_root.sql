b32760a5bc19e0303da0ababe2f375d73f0bcf77
Jakub Porebski <jakub_porebski@epam.com>
2023-06-23 12:23:14 +0200

    Added the docker-compose.yaml file
    Basic setting up a docker architecture.

---
+++ scripts/mysql_prepare_root.sql
@@ -0,0 +1,6 @@
+-- TODO: Maybe it will be beneficial in the foreseeable future. For now, let's leave it as a commented script.
+-- create user root@'%' identified by 'iamgroot:)';
+-- grant all privileges on *.* to root@'%' with grant option;
+-- flush privileges;
+
+
