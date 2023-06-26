0971ade251cbc458303f6283e1d33cae88890a06
Jakub Porebski <jakub_porebski@epam.com>
2023-06-26 13:32:40 +0200

    make_zip.sh
    Forgot about adding docker-compose.yaml :P So I fixed it.

--- make_zip.sh
+++ make_zip.sh
@@ -7,3 +7,4 @@ zip -9 -r plik.zip resource_service -x resource_service/target/\* -x resource_se
 zip -9 -r plik.zip song_service -x song_service/target/\* -x song_service/output.log -x song_service/run.pid
 zip -9 -r plik.zip scripts
 zip -9 -r plik.zip remove.sh run.sh stop.sh
+zip -9 -r plik.zip docker-compose.yaml
