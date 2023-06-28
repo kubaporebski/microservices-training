49bacad51ebd0218e66595ceb5c5e3a90b60ec46
Jakub Porebski <jakub_porebski@epam.com>
2023-06-28 10:12:08 +0200

    Task #2 cont.
    Here we have a working thing. For now there is only the sc_resource service available in the docker.
    
    TODO:
      - exposing ports, so sc_resource will be visible,
      - setting up sc_song service in the same way.

--- resource_service/src/main/java/jporebski/microservices/resource_service/ResourceServiceApplication.java
+++ resource_service/src/main/java/jporebski/microservices/resource_service/ResourceServiceApplication.java
@@ -1,15 +1,19 @@
 package jporebski.microservices.resource_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
+import java.util.Objects;
 
 @SpringBootApplication
 public class ResourceServiceApplication {
+
+	private final static Logger logger = LoggerFactory.getLogger(ResourceServiceApplication.class);
 
 	public static void main(String[] args) {
 
@@ -19,35 +23,46 @@ public class ResourceServiceApplication {
 			System.exit(1);
 	}
 
+	/**
+	 * Low-level (TCP socket) checking if connection to the database is possible.
+	 *
+	 * @return true - the db is accepting connections / false - otherwise
+	 */
 	private static boolean tryConnectToDB() {
-		var dbUrl = System.getenv("SC_RESOURCE_DB_URL").split(":");
-		var sa = new InetSocketAddress(dbUrl[0], Integer.parseInt(dbUrl[1]));
+		var databaseHost = System.getenv("SC_RESOURCE_DB_HOST");
+		var databasePort = Integer.parseInt(System.getenv("SC_RESOURCE_DB_PORT"));
+		var sa = new InetSocketAddress(databaseHost, databasePort);
 		var tries = 1;
-		var maxTries = 10;
+		var maxTries = Integer.parseInt(Objects.toString(System.getenv("SC_DB_RETRY_COUNT"), "15"));
+		var lastException = (Exception)null;
+
+		logger.info("Trying to connect to database at {}:{}", databaseHost, databasePort);
 
 		while (tries <= maxTries) {
-			System.out.printf("Trying (%d/%d) to connect do the database... ", tries, maxTries);
+			logger.warn("Try #{} out of {}... ", tries, maxTries);
 			try {
 				try (var tcp = new Socket()) {
 
 					tcp.connect(sa);
 					if (tcp.isConnected()) {
-						System.out.println("OK.");
+						logger.info("OK. Database connection possible.");
 						return true;
 					}
 
-				} catch (IOException e) {
-					System.out.printf("failed: %s \r\n", e.getMessage());
+				} catch (IOException ioEx) {
+					lastException = ioEx;
+					logger.warn("Database connection error occured: {}", ioEx.getMessage());
 					tries++;
 					Thread.sleep((long) (Math.random() * 1111 + 1111));
 				}
 			}
-			catch (Exception ex) {
-				ex.printStackTrace();
+			catch (Exception wtfEx) {
+				logger.error("Unexpected error", wtfEx);
+				return false;
 			}
 		}
 
-		System.err.printf("Could not obtain a connection to the database at %s.\r\n", System.getenv("SC_RESOURCE_DB_URL"));
+		logger.error("Could not obtain a connection to the database because the following error", lastException);
 		return false;
 	}
 }
