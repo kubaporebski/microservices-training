c0b60f3f463107cd7abfc6bb8785ebd1c505266d
Jakub Porebski <jakub_porebski@epam.com>
2023-10-20 15:11:51 +0200

    Task #3
    Another notable changes. Experimentation and testing.

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -eu
+_PATH=`pwd`
+
+echo Eureka Server...
+cd "$_PATH/eureka_service"
+mvn -DskipTests clean package
 
 echo Resource service...
-cd resource_service
+cd "$_PATH/resource_service"
 mvn -DskipTests clean package
 
 echo Song service...
-cd ..
-cd song_service
+cd "$_PATH/song_service"
 mvn -DskipTests clean package
 
-cd ..
+cd "$_PATH"
 echo OK.
 
 
