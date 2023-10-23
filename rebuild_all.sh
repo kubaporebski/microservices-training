1657fedd0e22639443bb276ac9e3a163ff2d7b87
Jakub Porebski <jakub_porebski@epam.com>
2023-10-23 12:45:23 +0200

    docker-compose.yaml optimization

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 set -eu
-_PATH=`pwd`
 
 echo Eureka Server...
-cd "$_PATH/eureka_service"
-mvn -DskipTests clean package
+mvn -f eureka_service/ -DskipTests clean package
 
 echo Resource service...
-cd "$_PATH/resource_service"
-mvn -DskipTests clean package
+mvn -f resource_service/ -DskipTests clean package
 
 echo Song service...
-cd "$_PATH/song_service"
-mvn -DskipTests clean package
+mvn -f song_service/ -DskipTests clean package
 
-cd "$_PATH"
 echo OK.
 
 
